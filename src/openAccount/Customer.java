ff733d6d6ea29b19b479d7eec599e8341555ce1f
Jaish_Chimnani <jaish.chimnani_cs19@gla.ac.in>
2021-05-22 10:36:25 +0530

    creating new customer account

--- src/openAccount/Customer.java
+++ src/openAccount/Customer.java
@@ -1,48 +1,49 @@
 package openAccount;
 
 public class Customer {
-    private static long accountNumber;
-    private static String nameOfCustomer;
-    private static byte age;
-    private static String gender;
-    private static long phoneNumber;
-    private static String address;
-    private static String nationality;
+    private long accountNumber;
+    private String nameOfCustomer;
+    private byte age;
+    private String gender;
+    private long phoneNumber;
+    private String address;
+    private String nationality;
 
-    public Customer(String nameOfCustomer, byte age, String gender, long phoneNumber, String address, String nationality) {
-        Customer.nameOfCustomer = nameOfCustomer;
-        Customer.age = age;
-        Customer.gender = gender;
-        Customer.phoneNumber = phoneNumber;
-        Customer.address = address;
-        Customer.nationality = nationality;
+    public Customer(long accountNumber, String nameOfCustomer, byte age, String gender, long phoneNumber, String address, String nationality) {
+        this.accountNumber = accountNumber;
+        this.nameOfCustomer = nameOfCustomer;
+        this.age = age;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.nationality = nationality;
     }
 
-    public static long getAccountNumber() {
+    public long getAccountNumber() {
         return accountNumber;
     }
 
-    public static String getNameOfCustomer() {
+    public String getNameOfCustomer() {
         return nameOfCustomer;
     }
 
-    public static byte getAge() {
+    public byte getAge() {
         return age;
     }
 
-    public static String getGender() {
+    public String getGender() {
         return gender;
     }
 
-    public static long getPhoneNumber() {
+    public long getPhoneNumber() {
         return phoneNumber;
     }
 
-    public static String getAddress() {
+    public String getAddress() {
         return address;
     }
 
-    public static String getNationality() {
+    public String getNationality() {
         return nationality;
     }
 }
